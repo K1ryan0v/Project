3e07182ad9a0402ba893918e93e52dbbb4b11f0c
Kiryanov <adward28rus@vk.com>
2022-05-08 23:06:43 +0200

    javascript1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB={
+  count:numberOfFilms,
+  movies:{},
+  actors:{},
+  genres:[],
+  privet:false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+      b = prompt('на сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов', ''),
+      d = prompt('на сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
